861d9b06e3486a28848bcd3c3384df61e9f57dbf
lrcouto <laurarccouto@gmail.com>
2021-10-09 01:16:54 -0300

    Done, just need to make it fancy.

---
+++ sources/actions.c
@@ -0,0 +1,73 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   actions.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lcouto <lcouto@student.42sp.org.br>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/08 20:15:05 by lcouto            #+#    #+#             */
+/*   Updated: 2021/10/09 00:53:10 by lcouto           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philo.h"
+
+void	release_forks(t_philo *philo)
+{
+	pthread_mutex_unlock(philo->left_fork);
+	pthread_mutex_lock(philo->print_lock);
+	output(timestamp(philo->session_start), philo->index, DROP_LEFT_FORK, 0);
+	pthread_mutex_unlock(philo->print_lock);
+	pthread_mutex_unlock(philo->right_fork);
+	pthread_mutex_lock(philo->print_lock);
+	output(timestamp(philo->session_start), philo->index, DROP_LEFT_FORK, 0);
+	pthread_mutex_unlock(philo->print_lock);
+	philo->meals_eaten += 1;
+}
+
+void	pick_up_forks(t_philo *philo)
+{
+	pthread_mutex_lock(philo->left_fork);
+	pthread_mutex_lock(philo->print_lock);
+	output(timestamp(philo->session_start), philo->index, PICK_LEFT_FORK, 0);
+	pthread_mutex_unlock(philo->print_lock);
+	pthread_mutex_lock(philo->right_fork);
+	pthread_mutex_lock(philo->print_lock);
+	output(timestamp(philo->session_start), philo->index, PICK_RIGHT_FORK, 0);
+	pthread_mutex_unlock(philo->print_lock);
+	philo->last_meal = timestamp(philo->session_start);
+}
+
+int	thinking(t_philo *philo)
+{
+	if (*philo->death == TRUE || is_philo_satiated(philo) == TRUE)
+		return (FALSE);
+	pthread_mutex_lock(philo->print_lock);
+	output(timestamp(philo->session_start), philo->index, THINKING, 0);
+	pthread_mutex_unlock(philo->print_lock);
+	return (TRUE);
+}
+
+int	sleeping(t_philo *philo)
+{
+	if (*philo->death == TRUE || is_philo_satiated(philo) == TRUE)
+		return (FALSE);
+	pthread_mutex_lock(philo->print_lock);
+	output(timestamp(philo->session_start), philo->index, SLEEPING, 0);
+	pthread_mutex_unlock(philo->print_lock);
+	wait_time_in_ms(philo->sleep_time);
+	return (TRUE);
+}
+
+int	eating(t_philo *philo)
+{
+	if (*philo->death == TRUE || is_philo_satiated(philo) == TRUE)
+		return (FALSE);
+	pick_up_forks(philo);
+	pthread_mutex_lock(philo->print_lock);
+	output(timestamp(philo->session_start), philo->index, EATING, 0);
+	pthread_mutex_unlock(philo->print_lock);
+	release_forks(philo);
+	wait_time_in_ms(philo->eat_time);
+	return (TRUE);
+}
---
+++ sources/routine.c
@@ -0,0 +1,72 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   routine.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lcouto <lcouto@student.42sp.org.br>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/08 20:37:48 by lcouto            #+#    #+#             */
+/*   Updated: 2021/10/09 01:11:17 by lcouto           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philo.h"
+
+int	is_philo_satiated(t_philo *philo)
+{
+	if (*philo->total_meals > 0 && philo->meals_eaten == *philo->total_meals)
+		return (TRUE);
+	return (FALSE);
+}
+
+static void	death_sequence(t_philo *philo)
+{
+	*philo->death = TRUE;
+	*philo->time_of_death = timestamp(philo->session_start);
+	*philo->who_is_dead = philo->index;
+}
+
+static void	*monitor_end(void *philo_pointer)
+{
+	t_philo	*philo;
+
+	philo = (t_philo *)philo_pointer;
+	while (1)
+	{
+		if (*philo->death == TRUE)
+		{
+			pthread_mutex_unlock(philo->left_fork);
+			pthread_mutex_unlock(philo->right_fork);
+			return ((void *)1);
+		}
+		if (is_philo_satiated(philo) == TRUE)
+			output(timestamp(philo->session_start), philo->index,
+				SATIATED, philo->meals_eaten);
+		if ((timestamp(philo->session_start) - philo->last_meal)
+			> philo->death_time && *philo->death == FALSE)
+		{
+			pthread_mutex_unlock(philo->left_fork);
+			pthread_mutex_unlock(philo->right_fork);
+			pthread_mutex_lock(philo->end_monitor);
+			death_sequence(philo);
+			pthread_mutex_unlock(philo->end_monitor);
+			return ((void *)1);
+		}
+	}
+}
+
+void	*routine(void *philo_pointer)
+{
+	t_philo		*philo;
+	pthread_t	end;
+
+	philo = (t_philo *)philo_pointer;
+	if (pthread_create(&end, NULL, &monitor_end, philo) != 0)
+		return ((void *)1);
+	pthread_detach(end);
+	if (philo->index % 2 == 0)
+		usleep(6000);
+	while (eating(philo) && sleeping(philo) && thinking(philo))
+		continue ;
+	return (NULL);
+}
